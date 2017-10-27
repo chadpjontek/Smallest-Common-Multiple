511262877d31817cc9ede3c4597f687cae072a16
Chad Pjontek <chadpjontek@gmail.com>
2017-10-27 14:37:16 -0400

    init commit

---
+++ Smallest-Common-Multiple.js
@@ -0,0 +1,29 @@
+function smallestCommons(arr) {
+  // Sort arr
+  arr.sort(function (a, b) {
+    return b - a;
+  });
+  // Make array with all numbers in the range (inclusive)
+  var rangeArr = [];
+  for (var i = arr[0]; i >= arr[1]; i--) {
+    rangeArr.push(i);
+  }
+  // Loop through the range, find the lcm by incrementing the multiplier until all numbers are divisible
+  var multiplier = 1;
+  var n = 0;
+  do {
+    if ((arr[0] * multiplier) % rangeArr[n] !== 0) {
+      multiplier++;
+      n = 0;
+    } else {
+      n++;
+    }
+  } while (n < rangeArr.length);
+  return multiplier * arr[0];
+}
+// Examples
+smallestCommons([1, 5]); // returns a number
+smallestCommons([1, 5]); // returns 60
+smallestCommons([5, 1]); // returns 60
+smallestCommons([1, 13]); // returns 360360
+smallestCommons([23, 18]); // returns 6056820
